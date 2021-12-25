2ee28a456a8e44d5ae757a72d14f686504058366
VanHau1088 <91788991+VanHau1088@users.noreply.github.com>
2021-12-25 09:04:38 +0700

    Update index.js

--- B2/index.js
+++ B2/index.js
@@ -9,30 +9,51 @@ let button_Mul = document.getElementById("btn_mul");
 let button_Div = document.getElementById("btn_div");
 let kq = document.getElementById("kq");
 
-    button_Add.addEventListener('click', () =>{
-       
-        let number1 = a.value;
-        let number2 = b.value;
-      
-            kq.innerHTML += add(parseInt((number1)), parseInt((number2))); 
-        
-        
-    });
+  
+button_Add.addEventListener('click' ,()=> {
+    let number1 = a.value;
+    let number2 = b.value;
 
-    button_Sub.addEventListener('click', () =>{
-        let number1 = a.value;
-        let number2 = b.value;
-        kq.innerHTML += sub(parseInt((number1)), parseInt((number2)))  ; 
-        });
+    if(isNaN(Number(number1)) || isNaN(Number(number2))){
+        alert('Number Not String');
+    }else{
+        kq.innerHTML += add(Number(number1), Number(number2));
+    }
+});
 
-    button_Mul.addEventListener('click', () =>{
-        let number1 = a.value;
-        let number2 = b.value;
-        kq.innerHTML += mul(parseInt((number1)), parseInt((number2))) ; 
-        });
+button_Sub.addEventListener('click' ,()=> {
+    let number1 = a.value;
+    let number2 = b.value;
 
-    button_Div.addEventListener('click', () =>{
-        let number1 = a.value;
-        let number2 = b.value;
-        kq.innerHTML += div(parseInt((number1)), parseInt((number2))) ; 
-        });
+    if(isNaN(Number(number1)) || isNaN(Number(number2))){
+        alert('Number Not String');
+    }else{
+        kq.innerHTML += sub(Number(number1), Number(number2));
+    }
+});
+
+
+button_Mul.addEventListener('click' ,()=> {
+    let number1 = a.value;
+    let number2 = b.value;
+
+    if(isNaN(Number(number1)) || isNaN(Number(number2))){
+        alert('Number Not String');
+    }else{
+        kq.innerHTML += mul(Number(number1), Number(number2));
+    }
+});
+
+
+button_Div.addEventListener('click' ,()=> {
+    let number1 = a.value;
+    let number2 = b.value;
+
+    if(isNaN(Number(number1)) || isNaN(Number(number2))){
+        alert('Number Not String');
+    }else{
+        kq.innerHTML += div(Number(number1), Number(number2));
+    }
+});
+
+
